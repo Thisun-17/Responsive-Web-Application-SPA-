d0e3c79520b1f2abaa571aea51734f78529985e3
Thisun-17 <thisun.20233173@iit.ac.lk>
2025-05-15 15:47:23 +0530

    refactor: Restructure App component layout to include Header and main content

--- estate-agent/src/App.jsx
+++ estate-agent/src/App.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
 import PropertyDetailsPage from "./pages/PropertyDetailsPage";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<SearchPage />} />
-        <Route path="/property/:id" element={<PropertyDetailsPage />} />
-      </Routes>
+      <div className="app-container">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<SearchPage />} />
+            <Route path="/property/:id" element={<PropertyDetailsPage />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 };
