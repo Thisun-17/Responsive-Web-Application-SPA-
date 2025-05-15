cc7c04ace9a25e75d4e6974ae057933b45f47081
Thisun-17 <thisun.20233173@iit.ac.lk>
2025-05-15 17:30:16 +0530

    feat: Update index.html title and add Font Awesome; include Footer component in App layout

--- estate-agent/src/App.jsx
+++ estate-agent/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
 import PropertyDetailsPage from "./pages/PropertyDetailsPage";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
             <Route path="/property/:id" element={<PropertyDetailsPage />} />
           </Routes>
         </main>
+        <Footer />
       </div>
     </Router>
   );
